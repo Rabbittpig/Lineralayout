6f1decc0a4eb81f7a69c42eccd1ffee683b36d6b
zwp <zhangwanping@tpson.cn>
2020-08-31 18:42:59 +0800

    add:Event基于回调的事件处理机制

--- app/src/main/java/com/example/lineralayout/EventActivity.java
+++ app/src/main/java/com/example/lineralayout/EventActivity.java
@@ -3,6 +3,8 @@ package com.example.lineralayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,21 +12,35 @@ import com.example.lineralayout.util.ToastUtil;
 
 public class EventActivity extends AppCompatActivity {
     private Button mBtnEvent;
+    private MyButton btnMy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
         mBtnEvent = findViewById(R.id.btn_event);
+        btnMy = findViewById(R.id.btn_my);
+        btnMy.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d("Listener","---onTouch---");
+                        break;
+
+                }
+                return false;
+            }
+        });
   /*      //内部类实现监听
         mBtnEvent.setOnClickListener(new OnClick());*/
         //匿名内部类实现监听
-    /*    mBtnEvent.setOnClickListener(new View.OnClickListener() {
+        mBtnEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ToastUtil.showMsg(EventActivity.this,"click...");
             }
-        });*/
+        });
    /* //通过事件源所在类实现
     mBtnEvent.setOnClickListener(EventActivity.this);*/
     /*    //通过外部类实现
@@ -49,6 +65,15 @@ public class EventActivity extends AppCompatActivity {
             }
         }
     }*/
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.d("Activity","---onTouchEvent---");
+                break;
+        }
+        return false;
     }
 }
